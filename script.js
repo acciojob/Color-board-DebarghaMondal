b1fd4767821adfae66cc0de992ad5b9a381c9177
Debargha Mondal <166617838+DebarghaMondal@users.noreply.github.com>
2024-08-01 11:54:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-//your JS code here. If required.
 let container = document.getElementById('container');
 
-for(let i = 0;i < 800; i++){
-	let square = document.createElement('div');
-	square.classList.add('square');
-	container.appendChild(square)
+for (let i = 0; i < 800; i++) {
+  let square = document.createElement('div');
+  square.classList.add('square');
+  container.appendChild(square);
 }
 
 let squares = document.querySelectorAll('.square');
 
 squares.forEach((square) => {
-square.addEventListener('mouseover',() =>{
-	square.style.backgroundColor = getRandomColor();
-	setTimeout(()=>{
-		square.style.backgroundColor= '';
-	},1000);
-})
+  square.addEventListener('mouseover', () => {
+    square.style.backgroundColor = getRandomColor();
+    setTimeout(() => {
+      square.style.backgroundColor = '';
+    }, 1000);
+  });
+});
 
-})
-
-function getRandomColor()
+function getRandomColor() {
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
