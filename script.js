0753a8754fa6b7eb73d38a3268f53a1d93e3516f
Debargha Mondal <166617838+DebarghaMondal@users.noreply.github.com>
2024-08-01 11:48:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+let container = document.getElementById('container');
+
+for(let i = 0;i < 800; i++){
+	let square = document.createElement('div');
+	square.classList>add('square');
+	container.appendChild(square)
+}
+
+let squares = document.querySelectorAll('.square');
+
+squares.forEach((square) => {
+square.addEventListener('mouseover',() =>{
+	square.style.backgroundColor = getRandomColor();
+	setTimeout(()=>{
+		square.style.backgroundColor= '';
+	},1000);
+})
+
+})
+
+function getRandomColor()
